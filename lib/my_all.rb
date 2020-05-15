3256b0534204863abbe37780029b7ebd64f532fc
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 04:05:20 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,13 +1,12 @@
 require 'pry'
 
-def my_all?(
-i = 0
-while i < 3
-  yield(1)
-  binding.pry
-  i = 0 + 1
-  end
-end
+
+
+
+
+
+
+
 #   while i < collection.length
 #     yield(collection[i])
 #     i = i + 1 
